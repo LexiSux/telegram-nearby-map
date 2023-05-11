6fb0c42c0ab2aa979da120b0d6c03ce9ea244060
LexiSux <72486618+LexiSux@users.noreply.github.com>
2023-05-11 01:33:44 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {} 
+
+
+// your telegram API credentials
+// please go to https://my.telegram.org to create your API credentials
+config.telegramApiId = 28934829;
+config.telegramApiHash = '547bac16c8a4cb5d8dab1079e7937c39';
+
+
+// server settings
+config.hostname = '127.0.0.1';
+config.port = 3000;
+
+
+module.exports = config;
